5eda719340352d572c794499fddbc8d66e97d4e9
Jaela Wesley <jcwesley93@gmail.com>
2019-02-19 12:25:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  counter = 0
+  modified_array = []
+  while counter < collection.length
+  modified_array << yield(collection[counter])
+  counter += 1
+  end
+  modified_array
+end
